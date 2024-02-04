30f0af7f80e1edb8335419c742cf62d18ff1848c
Pei-Wen Tang (Vincent) <redjackfred@gmail.com>
2024-02-03 19:55:29 -0800

    Initial commit

--- src/Leetcode912.java
+++ src/Leetcode912.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Leetcode912 {
+public class Leetcode912 {
+    public int[] sortArray(int[] nums) {
+        int minNum = Integer.MAX_VALUE;
+        int maxNum = Integer.MIN_VALUE;
+        for(int num: nums){
+            minNum = Math.min(minNum, num);
+            maxNum = Math.max(maxNum, num);
+        }
+        int[] count = new int[maxNum-minNum + 1];
+        for(int num: nums){
+            count[num - minNum]++;
+        }
+        int j = 0;
+        for(int i = 0; i < count.length; i++){
+            while(count[i] != 0 && j < nums.length){
+                nums[j++] = i + minNum;
+                count[i]--;
+            }
+        }
+        return nums;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Leetcode912 leetcode912 = new Leetcode912();
+        int[] nums = {5, 1, 1, 2, 0, 0, 6, -5, -4, -5, 5};
+        int[] ans = leetcode912.sortArray(nums);
+        System.out.println(Arrays.toString(ans));
     }
 }
